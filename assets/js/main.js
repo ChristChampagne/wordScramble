4e371c5de5bab0742fd47a32ba5c1af08a65dc95
Christian <frezzebrain42@gmail.com>
2023-10-12 02:32:53 -0600

    Tries, balls and mistakes counters now working.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,52 +4,122 @@ const words = [
     'bridge',
     'jump',
     'guitar',
+    'treasure',
+    'tank',
+    'pinacle',
+    'boxer'
 ];
 
 let word;
+let ArrMistakes = [];
+let ArrCorrect = [];
 let wordContainer = document.getElementById('word');
 let tries = document.getElementById('total-tries');
 let mistakes = document.getElementById('mistakes-chars');
 let charsContainer = document.getElementById('word-chars-container');
+let btnRandom = document.getElementById('btn-random');
+let btnReset = document.getElementById('btn-reset');
+let mistakesBalls = document.getElementsByName('mistake-ball')
 
-function randomWord() {
+function RandomWord() {
     return words[Math.floor(Math.random() * words.length)];
 }
-function randomizeWord(arrayWord) {
+function RandomizeWord(arrayWord) {
     for (i = arrayWord.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [arrayWord[i], arrayWord[j]] = [arrayWord[j], arrayWord[i]];
     }
     return arrayWord.toString();
 }
-
-
-function loadInterface(word) {
+function LoadInterface(word) {
     arrayWord = word.split("");
     arrayChars = word.split("").length;
     for (i = 1; i <= arrayChars; i++) {
-        charsContainer.innerHTML += '<input type="text" class="word-char" onInput="checkInput(' + i + ')" id="word-char-' + i + '" maxlength="2">';
+        charsContainer.innerHTML += '<input type="text" class="word-char" onkeyup="CheckInput(' + i + ')" id="word-char-' + i + '" maxlength="1">';
     }
-    wordContainer.innerHTML = randomizeWord(arrayWord).replace(/,/g, " ");
+    wordContainer.innerHTML = RandomizeWord(arrayWord).replace(/,/g, " ");
 }
-function startGame() {
-    word = randomWord();
-    loadInterface(word);
-}
-function checkInput(actualInput) {
-    wArray = word.split("")
+function CheckInput(actualInput) {
 
-    if(actualInput == 1){
-        document.getElementById('word-char-'+actualInput).value == wArray[0] ? console.log(true):console.log(false);
+
+
+    if (ArrMistakes.length >= 5) {
+        StartGame()
+    } else {
+
+        wArray = word.split("")
+        actualValue = document.getElementById('word-char-' + actualInput).value;
+
+        if (actualValue == "") {
+
+        } else {
+            if (actualValue == wArray[actualInput - 1]) {
+                ArrCorrect.push(actualValue);
+            } else {
+                ArrMistakes.push(actualValue)
+                mistakes.innerHTML = ArrMistakes;
+                tries.innerHTML = ArrMistakes.length;
+            }
+
+
+            focusInput = actualInput + 1;
+            if (actualInput == wArray.length) {
+                focusInput = 1;
+                document.getElementById('word-char-' + focusInput).focus();
+            } else {
+                focusInput = actualInput + 1;
+                document.getElementById('word-char-' + focusInput).focus();
+            }
+            if (ArrCorrect.toString() == wArray.toString()) {
+                alert('🎉 Success');
+                StartGame()
+            } else {
+                console.log('No :(')
+            }
+
+
+            setErrors()
+
+        }
     }
+}
 
+function setErrors() {
+    for (i = 1; i < ArrMistakes.length+1; i++) {
+        document.getElementById('ball-'+i).style.backgroundColor = "#7429C5";
+    }
+}
+function Clear() {
+    word = ""
+    charsContainer.innerHTML = "";
+    mistakes.innerHTML = "";
+    tries.innerHTML = "0";
+    ArrCorrect = []
+    ArrMistakes = [];
+
+    for (i = 1; i <= 5; i++) {
+        document.getElementById('ball-'+i).style.backgroundColor = "white";
+    }
     
-
-
-    focusInput = actualInput + 1;
-    document.getElementById('word-char-' + focusInput).focus();
 }
+function StartGame() {
+    Clear();
+    word = RandomWord();
+    LoadInterface(word);
+}
+btnRandom.addEventListener('click', () => {
+    StartGame();
+})
+btnReset.addEventListener('click', () => {
+    ArrMistakes = [];
+    mistakes.innerHTML = ArrMistakes;
+    tries.innerHTML = ArrMistakes.length;
 
-startGame();
+    for (i = 1; i <= 5; i++) {
+        document.getElementById('ball-'+i).style.backgroundColor = "white";
+    }
+})
+
+StartGame();
 
 
